3f09df29ea4c9ffdf2126390215788fee841f6b4
HauNguyen <hau.nguyen@groovetechnology.com>
2023-03-27 22:12:07 +0700

    delete vocabulary

---
+++ ELS.UseCase/Commands/Vocabularies/DeleteVocabulary/DeleteVocabularyCommand.cs
@@ -0,0 +1,6 @@
+﻿using ELS.UseCase.PluginInterfaces.Common;
+
+namespace ELS.UseCase.Commands.Vocabularies.DeleteVocabulary
+{
+    public record DeleteVocabularyCommand(long VocabularyId, string DeletedBy) : ICommand;
+}
---
+++ ELS.UseCase/Commands/Vocabularies/DeleteVocabulary/DeleteVocabularyCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using ELS.UseCase.PluginInterfaces.Common;
+using ELS.UseCase.PluginInterfaces.Repositories;
+using MediatR;
+
+namespace ELS.UseCase.Commands.Vocabularies.DeleteVocabulary
+{
+    public class DeleteVocabularyCommandHandler : ICommandHandler<DeleteVocabularyCommand>
+    {
+        private readonly IVocabularyRepository vocabularyRepository;
+
+        public DeleteVocabularyCommandHandler(IVocabularyRepository vocabularyRepository)
+        {
+            this.vocabularyRepository = vocabularyRepository;
+        }
+        public async Task<Unit> Handle(DeleteVocabularyCommand request, CancellationToken cancellationToken)
+        {
+            var vocb = await vocabularyRepository.GetAsync(x => x.Id == request.VocabularyId);
+
+            if (vocb != null)
+            {
+                vocb.Status = false;
+                vocb.UpdatedOn = DateTime.UtcNow;
+                vocb.UpdatedBy = request.DeletedBy;
+
+                await vocabularyRepository.UpdateAsync(vocb);
+            }
+            return Unit.Value;
+        }
+    }
+}
