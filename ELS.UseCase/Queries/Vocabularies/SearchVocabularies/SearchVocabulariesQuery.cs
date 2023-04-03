819a6900fc59151f28e1d47ee1a3d43d5c275ad1
HauNguyen <hau.nguyen@groovetechnology.com>
2023-04-03 21:01:21 +0700

    add search vocabularies component

--- ELS.UseCase/Queries/Vocabularies/SearchVocabularies/SearchVocabulariesQuery.cs
+++ ELS.UseCase/Queries/Vocabularies/SearchVocabularies/SearchVocabulariesQuery.cs
@@ -3,24 +3,5 @@ using ELS.UseCase.PluginInterfaces.Common;
 
 namespace ELS.UseCase.Queries.Vocabularies.SearchVocabularies
 {
-    public record SearchVocabulariesQuery : IQuery<List<Vocabulary>>
-    {
-        public SearchVocabulariesQuery(string term, string definition, WordClassType? classification, VocabularyLevel? level)
-        {
-            Term = term;
-            Definition = definition;
-            Classification = classification;
-            Level = level;
-        }
-
-        public SearchVocabulariesQuery()
-        {
-
-        }
-
-        public string Term { get; init; } = string.Empty;
-        public WordClassType? Classification { get; init; }
-        public string Definition { get; init; } = string.Empty;
-        public VocabularyLevel? Level { get; init; }
-    }
+    public record SearchVocabulariesQuery(string Term = "", string Definition = "", WordClassType? Classification = null, VocabularyLevel? Level = null) : IQuery<List<Vocabulary>>;
 }
